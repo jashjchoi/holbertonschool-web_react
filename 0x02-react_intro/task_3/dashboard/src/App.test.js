ef7e0529cbcbb19d905f9ee3dbf60bf70d11053d
jashjchoi <jashjchoi@gmail.com>
2022-02-08 18:48:03 +0000

    Add more test files for App and notifications

---
+++ 0x02-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,27 @@
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('Enzyme - Testing App.js Functions:', () => {
+  it('1. App running sucessfully without crashing ', (done) => {
+    expect(shallow(<App />).exists());
+    done();
+  });
+
+  it('2. verify App-header', (done) => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<header className='App-header' />))
+    done()
+  });
+
+  it('3. verify App-body', (done) => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<body className='App-body' />))
+    done();
+  });
+
+  it('4. verify App-footer', (done) => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<footer className='App-footer' />))
+    done();
+  });
+});
